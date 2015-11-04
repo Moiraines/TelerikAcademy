7bb22599347a03b7714541849ce23fea686b2a83
Lyubo <Lyubomir.Yonchev@gmail.com>
2015-11-04 13:00:06 +0200

    added Jedi Mediation task

---
+++ Homeworks/Data-Structures-And-Algorithms/Workshops/01. Jedi-Meditation/JediMeditation/StartUp.cs
@@ -0,0 +1,48 @@
+﻿namespace JediMeditation
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string line = Console.ReadLine();
+
+            var lineArr = line.Split(' ');
+
+            var masterQueue = new Queue<string>();
+            var knightQueue = new Queue<string>();
+            var padawanQueue = new Queue<string>();
+
+            string element;
+            for (int i = 0; i < n; i++)
+            {
+                element = lineArr[i];
+                switch (element[0])
+                {
+                    case'm':
+                        { masterQueue.Enqueue(element);}
+                        break;
+                    case 'k':
+                        { knightQueue.Enqueue(element); }
+                        break;
+                    case 'p':
+                        { padawanQueue.Enqueue(element); }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.Write(string.Join(" ", masterQueue));
+            Console.Write(" ");
+            Console.Write(string.Join(" ", knightQueue));
+            Console.Write(" ");
+            Console.Write(string.Join(" ", padawanQueue));
+            Console.WriteLine();
+        }
+    }
+}
