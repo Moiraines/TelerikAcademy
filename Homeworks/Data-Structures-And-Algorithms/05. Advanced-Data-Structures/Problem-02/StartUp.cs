e85af89ed44411e62df9dbeb7e7ca716323d82db
Lyubo <Lyubomir.Yonchev@gmail.com>
2015-11-09 11:32:41 +0200

    added Advanced Data Structures HW

---
+++ Homeworks/Data-Structures-And-Algorithms/05. Advanced-Data-Structures/Problem-02/StartUp.cs
@@ -0,0 +1,63 @@
+﻿namespace Problem_02
+{
+    using System;
+    using Wintellect.PowerCollections;
+
+    /*
+    Write a program to read a large collection of products (name + price) and efficiently find the first 20 products 
+    in the price range [a…b].
+        Test for 500 000 products and 10 000 price searches.
+        Hint: you may use OrderedBag<T> and sub-ranges.
+    */
+    public class StartUp
+    {
+        private const int ProductsCount = 500000;
+        private const int PriseSearches = 10000;
+        private const int MinPrice = 1;
+        private const int MaxPrice = 1000;
+        private static readonly Random RandomGenerator = new Random();
+
+        static void Main()
+        {
+            Console.WriteLine("Start generating collection at {0:hh:mm:ss}", DateTime.Now);
+            var collection = GenerateCollection();
+            Console.WriteLine("Start performing searches at {0:hh:mm:ss}", DateTime.Now);
+            PerformSearches(collection);
+            Console.WriteLine("Searches done at {0:hh:mm:ss}", DateTime.Now);
+        }
+
+        private static OrderedBag<Product> GenerateCollection()
+        {
+            var collection = new OrderedBag<Product>();
+
+            for (int i = 0; i < ProductsCount; i++)
+            {
+                var randomPrice = RandomGenerator.Next(MinPrice, MaxPrice + 1);
+                collection.Add(new Product("p" + i, randomPrice));
+            }
+
+            return collection;
+        }
+
+        private static void PerformSearches(OrderedBag<Product> collection)
+        {
+            for (int i = 0; i < PriseSearches; i++)
+            {
+                var rangeStart = RandomGenerator.Next(MinPrice, MaxPrice + 1);
+                var rangeEnd = RandomGenerator.Next(MinPrice, MaxPrice + 1);
+
+                if (rangeStart > rangeEnd)
+                {
+                    var swap = rangeStart;
+                    rangeStart = rangeEnd;
+                    rangeEnd = swap;
+                }
+
+                var results = collection.Range(new Product("From", rangeStart), true, new Product("To", rangeEnd), true);
+
+                // Only for verification that actual product ranges are returned.
+                // Console.WriteLine("Search from {0} to {1} returned {2} results", rangeStart, rangeEnd, results.Count());
+            }
+        }
+    }
+}
---
+++ Homeworks/Data-Structures-And-Algorithms/05. Advanced-Data-Structures/Problem-03/StartUp.cs
@@ -0,0 +1,53 @@
+﻿namespace Problem_03
+{
+    using System;
+
+    /*
+    Write a program that finds a set of words (e.g. 1000 words) in a large text (e.g. 100 MB text file).
+        Print how many times each word occurs in the text.
+        Hint: you may find a C# trie in Internet.
+    */
+    public class StartUp
+    {
+        public const int WordsToAdd = 1000000;
+        public const int WordsToSearch = 1000000;
+        public static readonly Random RandomGenerator = new Random();
+
+        public static void Main()
+        {
+            Console.WriteLine("Start generating words at {0:hh:mm:ss}", DateTime.Now);
+
+            var trie = new Trie();
+
+            for (int i = 0; i < WordsToAdd; i++)
+            {
+                string word = GetRandomWord();
+                trie.Add(word);
+            }
+
+            Console.WriteLine("Start searching words at {0:hh:mm:ss}", DateTime.Now);
+
+            for (int i = 0; i < WordsToSearch; i++)
+            {
+                string word = GetRandomWord();
+                trie.GetWordOccurance(word);
+            }
+
+            Console.WriteLine("Found {0} words at {1:hh:mm:ss} ", WordsToSearch, DateTime.Now);
+
+            Console.Write("Most common word: ");
+            Console.WriteLine(trie.GetMostCommonWord());
+        }
+
+        public static string GetRandomWord()
+        {
+            char[] newWord = new char[RandomGenerator.Next(5, 12)];
+            for (int i = 0; i < newWord.Length; i++)
+            {
+                newWord[i] = (char)RandomGenerator.Next(65, 91);
+            }
+
+            return new string(newWord);
+        }
+    }
+}
