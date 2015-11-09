9a69221b80468e98b187ca497a47343675314aab
Lyubo <Lyubomir.Yonchev@gmail.com>
2015-11-09 12:21:14 +0200

    Data Structure Efficiency HW added

---
+++ Homeworks/Data-Structures-And-Algorithms/06. Data-Structures-Efficiency/Problem-01/StartUp.cs
@@ -0,0 +1,73 @@
+﻿namespace Problem_01
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    /*
+    A text file students.txt holds information about students and their courses in the following format:
+    Using SortedDictionary<K,T> print the courses in alphabetical order and for each of them prints the students 
+    ordered by family and then by name:
+        Kiril  | Ivanov   | C#
+        Stefka | Nikolova | SQL
+        Stela  | Mineva   | Java
+        Milena | Petrova  | C#
+        Ivan   | Grigorov | C#
+        Ivan   | Kolev    | SQL
+        C#: Ivan Grigorov, Kiril Ivanov, Milena Petrova
+        Java: Stela Mineva
+        SQL: Ivan Kolev, Stefka Nikolova
+    */
+    public class StartUp
+    {
+        static void Main()
+        {
+            var courses = new SortedDictionary<string, SortedDictionary<string, SortedSet<string>>>();
+            using (StreamReader reader = new StreamReader(@"../../students.txt"))
+            {
+                var line = reader.ReadLine();
+                while (line != null)
+                {
+                    var lineParts = line.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+                    var name = lineParts[0].Trim();
+                    var familyName = lineParts[1].Trim();
+                    var course = lineParts[2].Trim();
+                    if (courses.ContainsKey(course))
+                    {
+                        if (courses[course].ContainsKey(familyName))
+                        {
+                            courses[course][familyName].Add(name);
+                        }
+                        else
+                        {
+                            courses[course].Add(familyName, new SortedSet<string>() { name });
+                        }
+                    }
+                    else
+                    {
+                        courses.Add(
+                            course,
+                            new SortedDictionary<string, SortedSet<string>>()
+                            {
+                                { familyName, new SortedSet<string>() { name } }
+                            });
+                    }
+
+                    line = reader.ReadLine();
+                }
+            }
+
+            foreach (var course in courses)
+            {
+                Console.WriteLine("Course {0}", course.Key);
+                foreach (var familyName in course.Value)
+                {
+                    foreach (var name in familyName.Value)
+                    {
+                        Console.WriteLine("\t{0} {1}", name, familyName.Key);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Homeworks/Data-Structures-And-Algorithms/06. Data-Structures-Efficiency/Problem-02/StartUp.cs
@@ -0,0 +1,47 @@
+﻿namespace Problem_02
+{
+    using System;
+    using Wintellect.PowerCollections;
+
+    /*
+    A large trade company has millions of articles, each described by barcode, vendor, title and price.
+    Implement a data structure to store them that allows fast retrieval of all articles in given price range [x…y].
+    Hint: use OrderedMultiDictionary<K,T> from Wintellect's Power Collections for .NET.
+    */
+    public class StartUp
+    {
+        private const int Records = 1000000;
+        private const int MinPrice = 1;
+        private const int MaxPrice = 1000;
+        private static readonly Random RandomGenerator = new Random();
+
+        public static void Main()
+        {
+            // String will simulate the class needed for article (barcode, vendor, title) since the order only needs to be by price
+            var dictionary = new OrderedMultiDictionary<int, string>(true);
+
+            Console.WriteLine("Start generating collection at {0:hh:mm:ss}", DateTime.Now);
+            for (int i = 0; i < Records; i++)
+            {
+                var randomPrice = RandomGenerator.Next(MinPrice, MaxPrice + 1);
+                dictionary.Add(randomPrice, "p" + 1);
+            }
+
+            Console.WriteLine("Start performing range search at {0:hh:mm:ss}", DateTime.Now);
+
+            var rangeStart = RandomGenerator.Next(MinPrice, MaxPrice + 1);
+            var rangeEnd = RandomGenerator.Next(MinPrice, MaxPrice + 1);
+
+            if (rangeStart > rangeEnd)
+            {
+                var swap = rangeStart;
+                rangeStart = rangeEnd;
+                rangeEnd = swap;
+            }
+
+            var results = dictionary.Range(rangeStart, true, rangeEnd, true).Values.Count;
+
+            Console.WriteLine("Searching done at {0:hh:mm:ss}\nFound {1} items in the price range [{2}..{3}]", DateTime.Now, results, rangeStart, rangeEnd);
+        }
+    }
+}
